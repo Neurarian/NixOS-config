611911bec3d0f77e9329c35f58ceac49e48d589f
Neurarian <110474238+Neurarian@users.noreply.github.com>
2024-10-31 21:35:53 +0100

    create proper module for configurable qemu gpu passtrough hook

--- hosts/common/optional/libvirt.nix
+++ hosts/common/optional/libvirt.nix
@@ -6,34 +6,96 @@
 }:
 {
   options = {
-    libvirt.enable = lib.mkEnableOption "Enable libvirt module";
+    libvirt = {
+      enable = lib.mkEnableOption "Enable libvirt module";
+
+      # Hook to detach & reattach GPU
+      qemuHook = {
+        enable = lib.mkEnableOption "Enable hook for GPU passthrough";
+        vmName = lib.mkOption {
+          type = lib.types.str;
+          default = "";
+          description = "The name of the VM to apply the hook to.";
+        };
+        pciDevices = lib.mkOption {
+          type = lib.types.listOf lib.types.str;
+          default = [ ];
+          description = "List of PCI devices to detach/reattach for GPU passthrough";
+        };
+        gpuModule = lib.mkOption {
+          type = lib.types.str;
+          default = "";
+          description = "The GPU module to be unloaded and reloaded.";
+        };
+        vfioModule = lib.mkOption {
+          type = lib.types.str;
+          default = "";
+          description = "The VFIO module to be loaded for passthrough.";
+        };
+      };
+
+    };
   };
 
-  config = lib.mkIf config.libvirt.enable {
+  config =
+    let
+      hookCfg = config.libvirt.qemuHook;
+    in
+    lib.mkIf config.libvirt.enable {
 
-    programs.virt-manager.enable = true;
+      programs.virt-manager.enable = true;
 
-    # Service to declaratively start VFIO networking
-    virsh_netstart_service.enable = true;
-    virtualisation.libvirtd = {
-      enable = true;
-      qemu = {
-        package = pkgs.qemu_kvm;
-        runAsRoot = true;
-        swtpm.enable = true;
-        ovmf = {
-          enable = true;
-          packages = [ pkgs.OVMFFull.fd ];
-          /*
-            packages = [
-              (pkgs.OVMF.override {
-                secureBoot = true;
-                tpmSupport = true;
-              }).fd
-            ];
-          */
+      # Service to declaratively start VFIO networking
+      virsh_netstart_service.enable = true;
+      virtualisation.libvirtd = {
+        enable = true;
+        qemu = {
+          package = pkgs.qemu_kvm;
+          runAsRoot = true;
+          swtpm.enable = true;
+          ovmf = {
+            enable = true;
+            packages = [ pkgs.OVMFFull.fd ];
+          };
+        };
+        hooks.qemu = lib.mkIf hookCfg.enable {
+          "gpuPassthrough" = pkgs.writers.writeBash "gpuPassthrough" ''
+            # Variables
+            GUEST_NAME="$1"
+            OPERATION="$2"
+            SUB_OPERATION="$3"
+
+            # Run commands when the VM is started/stopped.
+            if [ "$GUEST_NAME" == "${hookCfg.vmName}" ]; then
+              if [ "$OPERATION" == "prepare" ]; then
+                if [ "$SUB_OPERATION" == "begin" ]; then
+                  systemctl stop greetd
+                  sleep 4
+
+                  # Detach PCI devices
+                  ${lib.concatStringsSep "\n" (map (pci: "virsh nodedev-detach ${pci}") hookCfg.pciDevices)}
+
+                  # Unload GPU module and load VFIO module
+                  modprobe -r ${hookCfg.gpuModule}
+                  modprobe ${hookCfg.vfioModule}
+                fi
+              fi
+
+              if [ "$OPERATION" == "release" ]; then
+                if [ "$SUB_OPERATION" == "end" ]; then
+                  # Reattach PCI devices
+                  ${lib.concatStringsSep "\n" (map (pci: "virsh nodedev-reattach ${pci}") hookCfg.pciDevices)}
+
+                  # Unload VFIO module and reload GPU module
+                  modprobe -r ${hookCfg.vfioModule}
+                  modprobe ${hookCfg.gpuModule}
+
+                  systemctl start greetd
+                fi
+              fi
+            fi
+          '';
         };
       };
     };
-  };
 }
