97f43499f36c2ca00e215180ebcac293d29f1285
Neurarian <110474238+Neurarian@users.noreply.github.com>
2025-03-21 17:11:04 +0100

    update: firefox options

--- home/Liqyid/common/optional/desktop/firefox.nix
+++ home/Liqyid/common/optional/desktop/firefox.nix
@@ -23,7 +23,7 @@
           id = 0;
           name = "default";
           isDefault = true;
-          extensions = with inputs.firefox-addons.packages."${system}"; [
+          extensions.packages = with inputs.firefox-addons.packages."${system}"; [
             ublock-origin
             privacy-badger
             i-dont-care-about-cookies
@@ -34,47 +34,50 @@
             vimium
             firefox-color
           ];
-          bookmarks = [
-            {
-              name = "Toolbar";
-              toolbar = true;
-              bookmarks = [
-                {
-                  name = "Catppuccin-Mocha-Lavender";
-                  url = "https://color.firefox.com/?theme=XQAAAAJHBAAAAAAAAABBqYhm849SCicxcUcPX38oKRicm6da8pFtMcajvXaAE3RJ0F_F447xQs-L1kFlGgDKq4IIvWciiy4upusW7OvXIRinrLrwLvjXB37kvhN5ElayHo02fx3o8RrDShIhRpNiQMOdww5V2sCMLAfehhpWbjL_1RPuSDS6JMrP5SMm3V5s8DPdjrylB8odQkBKx3hwS8DfSgUd_K1gnYDiqF6FLqjZ1i5o2Ag7ndcuIMtTOff0Mv_AUAr9gmCgTwJLuHS5akRaMkSVVO8YgQjswELYw-q6z3M9DY23nuubC_GQIUqlw0_94uTV6vwegBVDkZbRRpVsBlfXUf_GqVF9q79Pr6uBA49roImTVMPyDZ9TZWf4oGlebH55k7lGonhR7tIVq6T0EitalyQmofr7ZRrkD9AZTe2f1aJuLnVsX_37dxUN2Qzo48s6AGZ7O1x-eVU1x4fGwGN3uMKhlk7umStxFC_xhSJTiVloH7_g2XYIb96FY63jjMAnWH3NlYewHQpDH4WEe-AcSxUy9IkyEnirYFd0aPe_x62ahv3L4HEH_8pylyA";
-                }
-                {
-                  name = "Catppuccin-Firefox";
-                  tags = [
-                    "theme"
-                    "firefox"
-                  ];
-                  url = "https://github.com/catppuccin/firefox";
-                }
-                {
-                  name = "MyNixOS";
-                  tags = [
-                    "search"
-                    "nixpkgs"
-                  ];
-                  url = "https://mynixos.com/nixpkgs/package";
-                }
-                {
-                  name = "Home Manager Options";
-                  tags = [
-                    "search"
-                    "home-manager-options"
-                  ];
-                  url = "https://home-manager-options.extranix.com/";
-                }
-                {
-                  name = "Github";
-                  tags = ["repository"];
-                  url = "https://github.com/dashboard";
-                }
-              ];
-            }
-          ];
+          bookmarks = {
+            force = true;
+            settings = [
+              {
+                name = "Toolbar";
+                toolbar = true;
+                bookmarks = [
+                  {
+                    name = "Catppuccin-Mocha-Lavender";
+                    url = "https://color.firefox.com/?theme=XQAAAAJHBAAAAAAAAABBqYhm849SCicxcUcPX38oKRicm6da8pFtMcajvXaAE3RJ0F_F447xQs-L1kFlGgDKq4IIvWciiy4upusW7OvXIRinrLrwLvjXB37kvhN5ElayHo02fx3o8RrDShIhRpNiQMOdww5V2sCMLAfehhpWbjL_1RPuSDS6JMrP5SMm3V5s8DPdjrylB8odQkBKx3hwS8DfSgUd_K1gnYDiqF6FLqjZ1i5o2Ag7ndcuIMtTOff0Mv_AUAr9gmCgTwJLuHS5akRaMkSVVO8YgQjswELYw-q6z3M9DY23nuubC_GQIUqlw0_94uTV6vwegBVDkZbRRpVsBlfXUf_GqVF9q79Pr6uBA49roImTVMPyDZ9TZWf4oGlebH55k7lGonhR7tIVq6T0EitalyQmofr7ZRrkD9AZTe2f1aJuLnVsX_37dxUN2Qzo48s6AGZ7O1x-eVU1x4fGwGN3uMKhlk7umStxFC_xhSJTiVloH7_g2XYIb96FY63jjMAnWH3NlYewHQpDH4WEe-AcSxUy9IkyEnirYFd0aPe_x62ahv3L4HEH_8pylyA";
+                  }
+                  {
+                    name = "Catppuccin-Firefox";
+                    tags = [
+                      "theme"
+                      "firefox"
+                    ];
+                    url = "https://github.com/catppuccin/firefox";
+                  }
+                  {
+                    name = "MyNixOS";
+                    tags = [
+                      "search"
+                      "nixpkgs"
+                    ];
+                    url = "https://mynixos.com/nixpkgs/package";
+                  }
+                  {
+                    name = "Home Manager Options";
+                    tags = [
+                      "search"
+                      "home-manager-options"
+                    ];
+                    url = "https://home-manager-options.extranix.com/";
+                  }
+                  {
+                    name = "Github";
+                    tags = ["repository"];
+                    url = "https://github.com/dashboard";
+                  }
+                ];
+              }
+            ];
+          };
           search = {
             force = true;
             default = "Searx";
